2de4d1370fda6e1806bf30f6c84bdac2b1caf64c
Florian <florian@florian.me.uk>
2023-09-18 21:46:57 +0100

    Add icon for executable

--- gnat-stimgen.py
+++ gnat-stimgen.py
@@ -1,7 +1,7 @@
 """Shorthand for the gnat-stimgen module for compilation with nuitka.
 
 Compile with nuitka using:
->>> py -m nuitka --onefile --include-package=gnat_stimgen.datasets gnat-stimgen.py
+>>> py -m nuitka --onefile --include-package=gnat_stimgen.datasets --windows-icon-from-ico=gnat-stimgen-icon.png gnat-stimgen.py
 """
 from gnat_stimgen.__main__ import main
 
